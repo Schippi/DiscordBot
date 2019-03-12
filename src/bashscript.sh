b58e591fd47f93ba22238572bc4ec60fb5645e07
theschippi@gmail.com <theschippi@gmail.com>
2019-03-12 22:05:38 +0000

    path in bashscript

--- src/bashscript.sh
+++ src/bashscript.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 COUNTER=0
 while [ $COUNTER  -le 10 ]; do
-	python3 -u DiscordBot.py tokens/live.token cfg |& tee -i -a ~/Discordbot.log
+	python3 -u DiscordBot.py ../tokens/live.token ../cfg |& tee -i -a ~/Discordbot.log
 	echo bash script loop $COUNTER
 	sleep 6m
 	let COUNTER=COUNTER+1
 done
 python3 ErrorMail.py $COUNTER
 while [ $COUNTER  -le 20 ]; do
-	python3 -u DiscordBot.py tokens/live.token cfg |& tee -i -a ~/Discordbot.log
+	python3 -u DiscordBot.py ../tokens/live.token ../cfg |& tee -i -a ~/Discordbot.log
 	echo bash script loop $COUNTER
 	sleep 6m
 	let COUNTER=COUNTER+1
