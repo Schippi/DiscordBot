c273eb9551e66f75749dc58bbd4443a6273fd34d
Schippi <theschippi@gmail.com>
2022-08-28 21:34:46 +0200

    beatsaber fixes

--- src/beatsaber/beatsaber_server.py
+++ src/beatsaber/beatsaber_server.py
@@ -51,7 +51,7 @@ def strToBoolOrNone(draw: str):
 
 async def download_all_loop(users):
     while True:
-        await download_all(users, False)
+        await download_all(users, True)
         for i in range(30):
             #print(i)
             await asyncio.sleep(60)
@@ -103,72 +103,75 @@ def data_to_db(data,cur):
 
 
 async def download_all(users, stopOnPgOne):
-    cur = util.DB.cursor()
-    async with aiohttp.ClientSession() as session:
-        for p in users:
-            os.makedirs('beatsaber/replays/%d' % p, exist_ok=True)
-            i = 1
-            while i > 0:
-                async with session.get('https://api.beatleader.xyz/player/%d/scores?page=%d' % (p, i)) as resp:
-                    if resp.status != 200:
-                        print('load page failed user: %d page: %d ' % (p, i))
-                        break
-                    print('loaded page user: %d page: %d ' % (p, i))
-                    i = i + 1
-                    data = await resp.json()
-                    dld = 0
-                    if len(data['data']) == 0:
-                        break
-                    for x in data['data']:
-                        replay_url = x['replay']
-                        if replay_url:
-                            replay_file_name = replay_url[replay_url.rindex('/')+1:]
-                            local_file_name = 'beatsaber/replays/%s/%s' % (p, replay_file_name)
-                            if not os.path.exists(local_file_name):
-                                data_to_db(x, cur)
-                                dld = dld + 1
-                                async with session.get(replay_url) as resp:
-                                    if resp.status != 200:
-                                        print(resp.status)
-                                        break
-                                    f = await aiofiles.open(local_file_name, mode='wb')
-                                    await f.write(await resp.read())
-                                    await f.close()
-                        else:
-                            print('REPLAY MISSING !?!? ' + str(x['id']))
-                    if dld == 0 and stopOnPgOne:
-                        break
+    with util.OpenCursor(util.DB) as cur:
+        async with aiohttp.ClientSession() as session:
+            for p in users:
+                os.makedirs('beatsaber/replays/%d' % p, exist_ok=True)
+                i = 1
+                while i > 0:
+                    async with session.get('https://api.beatleader.xyz/player/%d/scores?page=%d' % (p, i)) as resp:
+                        if resp.status != 200:
+                            print('load page failed user: %d page: %d ' % (p, i))
+                            break
+                        print('loaded page user: %d page: %d ' % (p, i))
+                        i = i + 1
+                        data = await resp.json()
+                        dld = 0
+                        if len(data['data']) == 0:
+                            break
+                        for x in data['data']:
+                            replay_url = x['replay']
+                            if replay_url:
+                                replay_file_name = replay_url[replay_url.rindex('/')+1:]
+                                local_file_name = 'beatsaber/replays/%s/%s' % (p, replay_file_name)
+                                if not os.path.exists(local_file_name):
+                                    data_to_db(x, cur)
+                                    dld = dld + 1
+                                    async with session.get(replay_url) as resp:
+                                        if resp.status != 200:
+                                            print(resp.status)
+                                            break
+                                        f = await aiofiles.open(local_file_name, mode='wb')
+                                        await f.write(await resp.read())
+                                        await f.close()
+                            else:
+                                print('REPLAY MISSING !?!? ' + str(x['id']))
+                        if dld == 0 and stopOnPgOne:
+                            break
 
 @bsroutes.get('/bs/songs')
 async def replay_handler(request):
-    cur = util.DB.cursor()
-    result = ''
-    with open('beatsaber/htdocs/list.html.part01', 'r') as f:
-        result = result + f.read();
-    with open('beatsaber/htdocs/list.html.part02', 'r') as f:
-        loopstr = f.read();
-    for row in cur.execute('SELECT * from bs_song '):
-        a = loopstr.replace('{song_id}', row['id']).replace('{cover}', row['cover_image']).replace('{mytxt}',row['name'] + ' ' + row['subname'])
-        result = result + a
-    with open('beatsaber/htdocs/list.html.part03', 'r') as f:
-        result = result + f.read();
+    with util.OpenCursor(util.DB) as cur:
+        result = ''
+        with open('beatsaber/htdocs/list.html.part01', 'r') as f:
+            result = result + f.read();
+        with open('beatsaber/htdocs/list.html.part02', 'r') as f:
+            loopstr = f.read();
+        for row in cur.execute('SELECT * from bs_song '):
+            a = loopstr.replace('{song_id}', row['id']).replace('{cover}', row['cover_image']).replace('{mytxt}',row['name'] + ' ' + row['subname'])
+            result = result + a
+        with open('beatsaber/htdocs/list.html.part03', 'r') as f:
+            result = result + f.read();
 
-    return web.Response(content_type='text/html', text=result)
+        return web.Response(content_type='text/html', text=result)
 
 @bsroutes.get('/bs/song/{song_id}')
 async def urlredirector(request):
     song_id = request.match_info['song_id']
 
-@bsroutes.get('/bs/replay')
+@bsroutes.get('/bs/replay/{score_id}')
 async def replay_handler(request):
-    insensitive_dict = {k.lower(): v for k, v in request.rel_url.query.items()}
-    score_id = insensitive_dict.get('score', None)
+    score_id = request.rel_url.query['score']
     if not score_id:
         return web.Response(status=400, reason='no score id')
     try:
         score_id = int(score_id)
     except:
         return web.Response(status=400, reason='score id invalid')
+
+
+
+
 
     async with aiohttp.ClientSession() as session:
         async with session.get('https://api.beatleader.xyz/score/%d' % score_id) as resp:
@@ -187,6 +190,16 @@ async def replay_handler(request):
                 await f.write(await resp.read())
                 await f.close()
     fig = read_map(local_file_name)
+
+    with util.OpenCursor(util.DB) as cur:
+        for row in cur.execute('select * from bs_replay br '
+                               'left join bs_song_diff bsf on br.id_diff = bsf.id '
+                               'left join bs_song bs on bsf.id_song = bs.id '
+                               'where br.id = ?',(score_id,)):
+            replay_url = row['replay']
+            fig.update_layout(
+                title=row['name']
+            )
     buf = io.StringIO()
     fig.write_html(buf)
 
