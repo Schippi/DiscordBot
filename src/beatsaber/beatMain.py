100f6667a6c816461b3f433fe0c21d41111919e2
Schippi <theschippi@gmail.com>
2022-09-11 20:43:21 +0200

    more graph!

--- src/beatsaber/beatMain.py
+++ src/beatsaber/beatMain.py
@@ -1,11 +1,33 @@
 
 import typing
 import os
-from Bsor import *
+from bsor.Bsor import *
 import plotly.express as px
 import plotly.graph_objects as go
 import aiohttp
 import asyncio
+
+import pprint
+
+# initialises a pretty printer
+pp = pprint.PrettyPrinter(indent=4)
+
+# We can now print with pp.pprint()
+
+# use copy to make sure we don't break the original figure dictionary
+import copy
+
+# the function we'll use to print figure dictionaries
+def pply(plyfig, data=False):
+
+    # the data option allows us to control whether the data is printed or not
+    if not data:
+        po = copy.deepcopy(plyfig)
+        po['data'] = [{i:trace[i] for i in trace if i not in ['x', 'y', 'z']} for trace in po['data']]
+
+        pp.pprint(po)
+    else:
+        pp.pprint(plyfig)
 
 def add_trace(fig,x,y,c,n):
     fig.add_trace(
@@ -20,10 +42,10 @@ def add_trace(fig,x,y,c,n):
 
 
 def read_map(filename, fig=None):
-    showLeft = True
-    showRight = True
-    showPause = True
-
+    showLeft = True #and False
+    showRight = True #and False
+    showPause = True and False
+    Angle = True# and False
 
     print('File name :    ', os.path.basename(filename))
     with open(filename, "rb") as f:
@@ -32,7 +54,13 @@ def read_map(filename, fig=None):
     print('finished ')
     suffix=''
     if not fig:
-        fig = go.Figure(layout_yaxis_range=[-1,116])
+        from plotly.subplots import make_subplots
+        fig = make_subplots(rows=2, cols=2,horizontal_spacing = 0.1,vertical_spacing = 0.05,  shared_xaxes=True, specs=[
+                                                                      [{"secondary_y": True},{'rowspan':2}],
+                                                                      [{},None]
+                                                                      ])
+
+        #fig = go.Figure(layout_yaxis_range=[-1,136])
         fig.update_yaxes(nticks=50)
         color_l = 'red'
         color_r = 'blue'
@@ -70,17 +98,154 @@ def read_map(filename, fig=None):
     #fig.update_xaxes(minor_dtick=50, minor_tick0=0, tickmode='linear')
 
     if showLeft:
-        x = [n.event_time for n in m.notes if n.colorType == 0]
+        x = [n.event_time for n in m.notes if n.colorType == 0 ]
         y = [n.score for n in m.notes if n.colorType == 0]
         add_trace(fig, x, y, color_l, 'Left' + suffix)
     if showRight:
-        x = [n.event_time for n in m.notes if n.colorType == 1]
-        y = [n.score for n in m.notes if n.colorType == 1]
+        x = [n.event_time for n in m.notes if n.colorType == 1 ]
+        y = [n.score for n in m.notes if n.colorType == 1 ]
         add_trace(fig, x, y, color_r, 'Right' + suffix)
+    if Angle:
+        x = [n.event_time for n in m.notes if hasattr(n,'cut') and n.cut.directionOk]
+        y = [abs(abs(n.cut.cutAngle)- 90) for n in m.notes if hasattr(n,'cut') and n.cut.directionOk]
+        add_trace(fig, x, y, 'green', 'Angle' + suffix)
+
+    bad,miss,bomb = [0,0],[0,0],[0,0]
+
+    for n in m.notes:
+        if n.event_type == NOTE_EVENT_BAD:
+            bad[n.colorType] = bad[n.colorType] + 1
+            pass
+        elif n.event_type == NOTE_EVENT_MISS:
+            miss[n.colorType] = bad[n.colorType] + 1
+            pass
+        elif n.event_type == NOTE_EVENT_BOMB:
+            bomb[n.colorType - 2] = bomb[n.colorType - 2] + 1
+            pass
+        if n.score > 0:
+            index = n.lineIndex + 4*n.noteLineLayer
+            if index > 11 or index < 0:
+                index = 0
+
+    combo = 0
+    score_events = [(n.event_time,n) for n in m.notes]
+    score_events.extend([(w.time,w) for w in m.walls])
+    sorted_events = sorted(score_events,key=lambda x: x[0])
+    max_score = 0
+    multiplier = 1
+    mul_progress = 0
+    mul_max_progress = 2
+    score = 0
+    note_cnt = 0
+    def inc_mul(i, progress, max_progress):
+        if i >= 8:
+            return i, progress, max_progress
+        if progress < max_progress:
+            progress = progress + 1
+        if progress >= max_progress:
+            i = i*2
+            progress = 0
+            max_progress = i * 2
+        return i, progress, max_progress
+
+    def dec_mul(i, progress, max_progress):
+        progress = 0
+        if i > 1:
+            i = i//2
+        max_progress = i * 2
+        return i, progress, max_progress
+    sco_x = []
+    scores =[]
+    max_scores = []
+    avg_left = []
+    avg_right = []
+    avg = []
+    avg_2 = []
+    note_cnt_left = 0
+    note_cnt_right = 0
+    score_no_mul = 0
+    max_score_no_mul = 0
+    max_no_mul_left = 0
+    max_no_mul_right = 0
+    score_no_mul_left = 0
+    score_no_mul_right = 0
+    angle_score = False
+    for idx,e in enumerate(sorted_events):
+        note_score = e[1].score
+        if angle_score and hasattr(e[1],'cut'):
+            note_score = note_score + 35 * (1-(abs(abs(n.cut.cutAngle)- 90) / 90))
+
+        if isinstance(e[1],Note):
+            note_cnt = note_cnt + 1
+            max_mul = 8 if note_cnt > 8+4+1 else 4 if note_cnt > 4+1 else 2 if note_cnt > 1 else 1
+            if e[1].scoringType == NOTE_SCORE_TYPE_BURSTSLIDERELEMENT:
+                max_score = max_score + max_mul * 20
+                max_score_no_mul = max_score_no_mul + 20
+                max_note = 20
+            else:
+                max_note = 115 if not angle_score else 150
+                max_score = max_score + max_mul * max_note
+                max_score_no_mul = max_score_no_mul + max_note
+
+            if e[1].colorType == 0:
+                max_no_mul_left = max_no_mul_left + max_note
+                score_no_mul_left = score_no_mul_left + note_score
+                note_cnt_left = note_cnt_left + 1
+                avg_left.append(score_no_mul_left / max_no_mul_left)
+                avg_right.append(1 if len(avg_right) == 0 else avg_right[-1])
+            if e[1].colorType == 1:
+                max_no_mul_right = max_no_mul_right + max_note
+                score_no_mul_right = score_no_mul_right + note_score
+                note_cnt_right = note_cnt_right + 1
+                avg_right.append(score_no_mul_right / max_no_mul_right)
+                avg_left.append(1 if len(avg_left) == 0 else avg_left[-1])
+
+        if isinstance(e[1],Wall) or isinstance(e[1],Note) and e[1].score == 0:
+            multiplier,mul_progress,mul_max_progress = dec_mul(multiplier,mul_progress,mul_max_progress)
+            combo = 0
+        else:
+            multiplier,mul_progress,mul_max_progress = inc_mul(multiplier,mul_progress,mul_max_progress)
+
+            combo = combo + 1
+            score = score + multiplier * note_score
+            score_no_mul = score_no_mul + note_score
+        avg.append(score_no_mul / max_score_no_mul)
+        s = sum(avg) / len(avg)
+        #avg_2.append(s)
+        sco_x.append(e[1].event_time)
+        scores.append(score)
+        max_scores.append(max_score)
+    percent = score / max_score
+    print('%d / %d' % (score, max_score))
+    print('%.2f' %(percent*100))
+    import plotly.graph_objects as go
+
+    fig.add_trace(go.Scatter(x=sco_x, y=max_scores, name='Max Score'),1,2)
+    fig.add_trace(go.Scatter(x=sco_x, y=[n*percent for n in max_scores], name='Your Avg Score'),1,2)
+    fig.add_trace(go.Scatter(x=sco_x, y=scores, name='Score'),1,2)
+
+    #percent_scores = []
+    #for i,j in zip(scores,max_scores):
+    #    percent_scores.append(i / j)
+    #fig.add_trace(go.Scatter(x=sco_x, y=percent_scores, name='percent'),2,1)
+    fig.add_trace(go.Scatter(x=sco_x, y=avg, name='avg', marker=dict(
+        color='brown',
+    )),2,1)
+    fig.add_trace(go.Scatter(x=sco_x, y=avg, name='avg', marker=dict(
+        color='brown',
+    )),secondary_y=True,row=1,col=1)
+    #fig.add_trace(go.Scatter(x=sco_x, y=avg_2, name='avg_2'),2,1)
+    fig.add_trace(go.Scatter(x=sco_x, y=avg_left, name='avg_left', marker=dict(
+        color='red',
+    )),2,1)
+    fig.add_trace(go.Scatter(x=sco_x, y=avg_right, name='avg_right', marker=dict(
+        color='blue',
+    )),2,1)
+
 
     # https://stackoverflow.com/questions/65941253/plotly-how-to-toggle-traces-with-a-button-similar-to-clicking-them-in-legend
     #fig= px.scatter(x=x, y=y, color=c, title=filename)
-
+    fig.update_layout(template='plotly_dark')
     return fig
 
 
@@ -88,7 +253,7 @@ if __name__ == '__main__':
     #https://api.beatleader.xyz/player/4476/scores?page=2
     #https://github.com/BeatLeader/BS-Open-Replay
     #filename = 'testData/4476-ExpertPlus-Standard-D04138A245357F230A3BFC568DE9AF89D4FAC687.bsor'
-    filename = 'testData/4476-ExpertPlus-Standard-19.bsor'
+    filename = 'D:/_TMP/Lady.bsor'
     #False True
     score_id = 890820
     fig = read_map(filename)
